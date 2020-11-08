f2ad6f92b584aad5afbeebf1f8a9833868a9be62
David Erickson <daviderickson@davids-mbp.lan>
2020-11-08 06:07:43 -0500

    fixed image names in gridimages, mapped over images in ShowImage to return grid.

--- src/components/ShowImage.js
+++ src/components/ShowImage.js
@@ -2,15 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+margin-top: 2rem;
+margin-bottom: 2rem;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-column-gap: 1rem;
+grid-row-gap: 1rem;
+
   .grid-image{
-    width: 80%;
+    width: 100%;
+        height: 100%;
+    object-fit: cover;
   }
 `
-
-const ShowImage = ({gridimage01}) => {
+//To Do map array of images ot make grid
+const ShowImage = ({images}) => {
+  console.log(images);
+  const gridToRender = images.map((image, index) => {
+  return <div key={index}>
+    <img src={image} className="grid-image"/>
+  </div>
+  })
     return (
         <Wrapper>
-                    <img src={gridimage01} className="grid-image"/>
+          {gridToRender}
         </Wrapper>
     )
 }
