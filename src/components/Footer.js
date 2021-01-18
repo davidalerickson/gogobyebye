f2e31449a5205d2f40bc7cedbee4f262249545bd
David Erickson <davidalerickson@hotmail.com>
2021-01-18 07:54:13 -0500

    Refactored Footer Class from Main.css to be styled component in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,54 @@
 import React from 'react'
 import SocialLinks from '../constants/socialLinks'
+import styled from 'styled-components';
+
+const FooterWrapper = styled.div`
+  .footer {
+    height: 7rem;
+    text-align: center;
+    background: var(--clr-black);
+    display: grid;
+    place-items: center;
+  }
+  .footer-icons {
+    display: flex;
+    justify-content: center;
+    line-height: 1;
+    margin-bottom: 0.5rem;
+  }
+  .footer-icons a {
+    font-size: 1.5rem;
+    margin: 0 0.5rem;
+    transition: var(--transition);
+  }
+  .footer-icons li {
+    transition: var(--transition);
+  }
+  .footer-icons li:hover {
+    transform: translateY(-10%);
+  }
+  .footer p {
+    margin-bottom: 0;
+    color: var(--clr-white);
+    text-transform: uppercase;
+    letter-spacing: var(--spacing);
+  }
+
+`
+
 const Footer = () => {
-  return <footer className="footer">
-    <div>
-      <SocialLinks styleClass="footer-icons"/>
-      <p>
-        &copy;{new Date().getFullYear()} GoGoByeBye and David Erickson. All rights reserved
-      </p>
-    </div>
-  </footer>
+  return (
+  <FooterWrapper>
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-icons"/>
+        <p>
+          &copy;{new Date().getFullYear()} GoGoByeBye and David Erickson. All rights reserved
+        </p>
+      </div>
+    </footer>
+  </FooterWrapper>
+  )
 }
 
 export default Footer
