1987d5d00d46659d3737b4232953793d94d9c65e
David Erickson <davidalerickson@hotmail.com>
2021-01-19 09:28:33 -0500

    Deleted Hero Comp and styles. Refactored Sidebar styles to be in component not Main.css

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,22 +1,58 @@
 import React from 'react'
 import Post from './Post'
 import Banner from '../Banner'
+import styled from 'styled-components';
+
+const PostsWrapper = styled.div`
+  .posts {
+    width: 85vw;
+    max-width: var(--max-width);
+    margin: 0 auto;
+    margin-bottom: 4rem;
+  }
+  .posts-title {
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #e12d39;
+    font-size: 1.25rem;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  @media screen and (min-width: 992px) {
+    .posts {
+      width: 92vw;
+    }
+  }
+  @media screen and (min-width: 1170px) {
+    .posts-center {
+      display: grid;
+      grid-template-columns: 1fr 250px;
+      column-gap: 1rem;
+    }
+  }
+`
+
 const Posts = ({posts, title}) => {
-  return <section className="posts">
-    <h3 className="posts-title">{title}</h3>
-    <div className="posts-center">
-      {/* posts column */}
-      <article>
-        {posts.map(post => {
-          return <Post key={post.id} {...post} />
-        })}
-      </article>
-      {/* banner column */}
-      <article>
-        <Banner/>
-      </article>
-    </div>
-  </section>
+  return (
+    <PostsWrapper>
+    <section className="posts">
+      <h3 className="posts-title">{title}</h3>
+      <div className="posts-center">
+        {/* posts column */}
+        <article>
+          {posts.map(post => {
+            return <Post key={post.id} {...post} />
+          })}
+        </article>
+        {/* banner column */}
+        <article>
+          <Banner/>
+        </article>
+      </div>
+    </section>
+    </PostsWrapper>
+  )
 }
 
 export default Posts
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,17 +2,84 @@ import React from 'react'
 import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
+import styled from 'styled-components';
+
+const SidebarWrapper = styled.div`
+
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 75%;
+  height: 100%;
+  background: var(--clr-grey-10rgba);
+  transition: var(--transition);
+  display: grid;
+  align-items: center;
+  padding-left: 2rem;
+  z-index: 999;
+  transform: translateX(-100%);
+  visibility: hidden;
+}
+.showSidebar {
+  transform: translateX(0);
+  visibility: visible;
+}
+.sidebar-links li {
+  margin-bottom: 1.5rem;
+}
+.sidebar-links .page-link {
+  font-size: 1.5rem;
+  display: block;
+  color: var(--clr-grey-1);
+  font-weight: 700;
+}
+.sidebar .categories {
+  flex-direction: column;
+  margin: 1rem 0;
+}
+.sidebar .category {
+  color: var(--clr-grey-5);
+  font-size: 1rem;
+  font-weight: 700;
+  text-transform: capitalize;
+  padding-left: 1rem;
+  display: block;
+  cursor: pointer;
+}
+
+@media screen and (min-width: 800px) {
+  .sidebar {
+    transform: translateX(-100%);
+  }
+}
+.close-btn {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  background: transparent;
+  border: transparent;
+  font-size: 3rem;
+  cursor: pointer;
+}
+`
+
 const Sidebar = ({isOpen, toggle}) => {
-  return <aside className={`sidebar ${isOpen ? 'showSidebar' : ''}`}>
-    <button className="close-btn" onClick={toggle}>
-      <IoMdClose/>
-    </button>
-    <div className="sidebar-container">
-      <Links styleClass="sidebar-links">
-        <Categories/>
-      </Links>
-    </div>
-  </aside>
+  return(
+    <SidebarWrapper>
+      <aside className={`sidebar ${isOpen ? 'showSidebar' : ''}`}>
+        <button className="close-btn" onClick={toggle}>
+          <IoMdClose/>
+        </button>
+        <div className="sidebar-container">
+          <Links styleClass="sidebar-links">
+            <Categories/>
+          </Links>
+        </div>
+      </aside>
+    </SidebarWrapper>
+  )
 }
 
 export default Sidebar
