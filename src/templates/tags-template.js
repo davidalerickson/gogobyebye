7a2a6ba9edcc4caca4ee20d8798365ab8bf129ad
David Erickson <davidalerickson@hotmail.com>
2021-03-08 11:24:15 -0500

    Add getTags query to tags-template and use tag variable to show posts with that tag

--- src/templates/tags-template.js
+++ src/templates/tags-template.js
@@ -6,38 +6,37 @@ import { graphql } from 'gatsby'
 const TagTemplate = (props) => {
   console.log(props);
   const {pageContext:{tag}} = props
-  // const {data:{categories:{nodes:posts}}} = props
+  const {data:{tags:{nodes:posts}}} = props
   return <Layout>
-    <h1>Hello from Tag template - {tag}</h1>
-    {/* <Posts posts={posts} title={`category/${category}`}/> */}
+    <Posts posts={posts} title={`tag - ${tag}`}/>
   </Layout>
 }
 
-// export const query = graphql`
-// query GetCategories($category: String) {
-//   categories: allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: {frontmatter: {category: {eq: $category}}}) {
-//     nodes {
-//       frontmatter {
-//         title
-//         author
-//         category
-//         date(formatString: "MMMM Do, YYYY")
-//         slug
-//         readTime
-//         image {
-//           childImageSharp {
-//             fluid {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//         }
-//       }
-//       id
-//       excerpt
-//     }
-//   }
-// }
+export const query = graphql`
+query GetTags($tag: String) {
+  tags: allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: {frontmatter: {tags: {eq: $tag}}}) {
+    nodes {
+      frontmatter {
+        title
+        author
+        category
+        date(formatString: "MMMM Do, YYYY")
+        slug
+        readTime
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      id
+      excerpt
+    }
+  }
+}
 
-// `
+`
 
 export default TagTemplate
