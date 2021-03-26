76c870b8dcbe8124e9ba96ea1855ece46c9a4d6b
David Erickson <davidalerickson@hotmail.com>
2021-03-25 23:05:00 -0400

    removed unused components from NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typoography from "@material-ui/core/Typography";
 import HideOnScroll from "../components/Widgets/HideOnScroll";
 import HeaderAnimation from "../components/HeaderAnimation";
 import { FaBars } from "react-icons/fa";
